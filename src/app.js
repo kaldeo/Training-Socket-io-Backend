cbba676a83bbb3d88eb49290b1b9efa0846e2635
kaldeo <gabin.freval@gmail.com>
2025-11-21 11:54:57 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const http = require('http')
+const { Server } = require('socket.io')
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`http://localhost:${port}`))
+const app = express()
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001", // Remplacez par l'URL de votre client
+    methods: ["GET", "POST"]
+  }
+});
+
+const port = 3000
+server.listen(port, () => console.log(`http://localhost:${port}`))
+
+
+io.on('connection', (socket) => {
+  console.log(`Un utilisateur s'est connecté : ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`L'utilisateur s'est déconnecté : ${socket.id}`);
+  });
+});
